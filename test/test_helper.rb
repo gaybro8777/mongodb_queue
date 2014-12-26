75a10e50126ba06d4bd0f776c92756c0988edd34
Jesse Bowes <jbowes@dashingrocket.com>
2014-12-25 19:54:38 -0700

    Generate HTML and XML Reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,10 @@
 require 'simplecov'
-require 'simplecov-csv'
 require 'simplecov-cobertura'
 
-#SimpleCov.formatters = [
-#    SimpleCov::Formatter::HTMLFormatter,
-#    SimpleCov::Formatter::CSVFormatter,
-#    SimpleCov::Formatter::CoberturaFormatter
-#]
-
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::CoberturaFormatter
+]
 
 SimpleCov.coverage_dir(ENV['COVERAGE_REPORTS'])
 SimpleCov.start
